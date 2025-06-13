1cbc0e374b3314b9f07e8489fd79c1a67b844d4c
Daniel Carlström Schad <7188700+Dod12@users.noreply.github.com>
2025-06-13 08:18:05 +0200

    Move collect_predictions to utils

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ from losses import (
     sample_similarity_loss,
     roi_similarity_loss
 )
-from utils import save_initial_state, load_initial_state, set_seed, log_model_params
+from utils import save_initial_state, load_initial_state, set_seed, log_model_params, collect_predictions
 from config import Config
 from viz import (
     load_and_label_atlas,
@@ -41,48 +41,6 @@ from viz import (
 
 import nibabel as nib
 from nilearn.maskers import NiftiLabelsMasker
-
-
-def collect_predictions(loader, model, device):
-    """
-    Run model over `loader` (no grad) and return:
-        fmri_true  : list of (T, V) arrays per subject in order
-        fmri_pred  : list of (T, V) arrays per subject in same order
-        subj_order : list of subject IDs as strings ("01", "02", ...)
-        atlas_paths: list of atlas paths (looked up from dataset samples)
-    Assumes each batch contains a single subject only (Algonauts starter).
-    """
-    model.eval()
-    subj_to_true = defaultdict(list)
-    subj_to_pred = defaultdict(list)
-    subj_to_atlas = {}
-    sid_map = {v: k for k, v in loader.dataset.subject_name_id_dict.items()}
-    with torch.no_grad():
-        for batch in loader:
-            subj_ids = batch["subject_ids"]      # tensor shape (B,)
-            run_ids  = batch["run_ids"]          # tensor shape (B,)
-            fmri     = batch["fmri"].to(device)
-            attn     = batch["attention_masks"].to(device)
-            run_ids  = batch["run_ids"]
-            feats    = {k: batch[k].to(device) for k in loader.dataset.modalities}
-
-            pred = model(feats, subj_ids, run_ids, attn)
-
-            for i, sid in enumerate(subj_ids):
-                sid = sid_map[sid]
-                subj_to_true[sid].append(fmri[i].cpu().numpy())
-                subj_to_pred[sid].append(pred[i].cpu().numpy())
-                if sid not in subj_to_atlas:
-                    atlas_path = loader.dataset.samples[0]["subject_atlas"].format(subject=sid)
-                    subj_to_atlas[sid] = atlas_path
-
-    fmri_true, fmri_pred, subj_order, atlas_paths = [], [], [], []
-    for sid in sorted(subj_to_true.keys()):
-        fmri_true.append(np.concatenate(subj_to_true[sid], axis=0))
-        fmri_pred.append(np.concatenate(subj_to_pred[sid], axis=0))
-        subj_order.append(sid)
-        atlas_paths.append(subj_to_atlas[sid])
-    return fmri_true, fmri_pred, subj_order, atlas_paths
 
 
 def parse_args():
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import random
 import wandb
 import numpy as np
@@ -34,3 +35,44 @@ def load_initial_state(model, path="initial_model.pt", random_path="initial_rand
     torch.set_rng_state(random_state["torch"])
     if torch.cuda.is_available() and random_state["cuda"] is not None:
         torch.cuda.set_rng_state_all(random_state["cuda"])
+
+def collect_predictions(loader, model, device):
+    """
+    Run model over `loader` (no grad) and return:
+        fmri_true  : list of (T, V) arrays per subject in order
+        fmri_pred  : list of (T, V) arrays per subject in same order
+        subj_order : list of subject IDs as strings ("01", "02", ...)
+        atlas_paths: list of atlas paths (looked up from dataset samples)
+    Assumes each batch contains a single subject only (Algonauts starter).
+    """
+    model.eval()
+    subj_to_true = defaultdict(list)
+    subj_to_pred = defaultdict(list)
+    subj_to_atlas = {}
+    sid_map = {v: k for k, v in loader.dataset.subject_name_id_dict.items()}
+    with torch.no_grad():
+        for batch in loader:
+            subj_ids = batch["subject_ids"]      # tensor shape (B,)
+            run_ids  = batch["run_ids"]          # tensor shape (B,)
+            fmri     = batch["fmri"].to(device)
+            attn     = batch["attention_masks"].to(device)
+            run_ids  = batch["run_ids"]
+            feats    = {k: batch[k].to(device) for k in loader.dataset.modalities}
+
+            pred = model(feats, subj_ids, run_ids, attn)
+
+            for i, sid in enumerate(subj_ids):
+                sid = sid_map[sid]
+                subj_to_true[sid].append(fmri[i].cpu().numpy())
+                subj_to_pred[sid].append(pred[i].cpu().numpy())
+                if sid not in subj_to_atlas:
+                    atlas_path = loader.dataset.samples[0]["subject_atlas"].format(subject=sid)
+                    subj_to_atlas[sid] = atlas_path
+
+    fmri_true, fmri_pred, subj_order, atlas_paths = [], [], [], []
+    for sid in sorted(subj_to_true.keys()):
+        fmri_true.append(np.concatenate(subj_to_true[sid], axis=0))
+        fmri_pred.append(np.concatenate(subj_to_pred[sid], axis=0))
+        subj_order.append(sid)
+        atlas_paths.append(subj_to_atlas[sid])
+    return fmri_true, fmri_pred, subj_order, atlas_paths

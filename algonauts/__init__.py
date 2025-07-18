24d3106e89026bf8f103e5e5b04d60bb94db488c
Daniel Carlström Schad <7188700+Dod12@users.noreply.github.com>
2025-07-18 16:18:34 +0200

    Added init imports

--- algonauts/__init__.py
+++ algonauts/__init__.py
@@ -1,2 +1,16 @@
+from .utils import Config, logger, viz
 
+from . import data, models, utils, cli
 
+from .training import losses
+
+__all__ = [
+    "Config",
+    "logger",
+    "viz",
+    "data",
+    "models",
+    "utils",
+    "cli",
+    "losses",
+]

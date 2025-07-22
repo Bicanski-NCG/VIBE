fd397e48edb3bb3e0e021d7cc7adfc42e6f85cdf
Daniel Carlström Schad <7188700+Dod12@users.noreply.github.com>
2025-07-22 20:54:37 +0200

    Add model1 params again

--- scripts/fit_all.sh
+++ scripts/fit_all.sh
@@ -7,6 +7,6 @@
 echo "Starting fitting process for all models..."
 sbatch --array=0-19 scripts/fit.sh --no_diagnostics --name model1 --features configs/features_model1.yaml --params configs/params_model1.yaml --output_dir $OUTPUTS_DIR
 sbatch --array=0-19 scripts/fit.sh --no_diagnostics --name model1_fr --features configs/features_model1_fr.yaml --params configs/params_model1_fr.yaml --output_dir $OUTPUTS_DIR
-sbatch --array=0-19 scripts/fit.sh --no_diagnostics --name model_visual --features configs/features_model_visual.yaml --params configs/params_model_visual.yaml --output_dir $OUTPUTS_DIR
-sbatch --array=0-19 scripts/fit.sh --no_diagnostics --name model_default --features configs/features_model_default.yaml --params configs/params_model_default.yaml --output_dir $OUTPUTS_DIR
+sbatch --array=0-19 scripts/fit.sh --no_diagnostics --name model_visual --features configs/features_visual.yaml --params configs/params_visual.yaml --output_dir $OUTPUTS_DIR
+sbatch --array=0-19 scripts/fit.sh --no_diagnostics --name model_default --features configs/features_default.yaml --params configs/params_default.yaml --output_dir $OUTPUTS_DIR
 echo "Fitting process initiated. Check the job scheduler for progress."
